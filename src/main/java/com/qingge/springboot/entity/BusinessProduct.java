c366dbaa402c0b851e7e40b47624e97c524c2665
longdengyu <464050079@qq.com>
2022-06-09 13:52:55 +0800

    Update: BusinessProduct的api设置

---
+++ src/main/java/com/qingge/springboot/entity/BusinessProduct.java
@@ -0,0 +1,35 @@
+package com.qingge.springboot.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author longdengyu
+ * @since 2022-06-09
+ */
+@Getter
+@Setter
+  @TableName("business_product")
+@ApiModel(value = "BusinessProduct对象", description = "")
+public class BusinessProduct implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+      @TableId(value = "user_id", type = IdType.AUTO)
+      private Integer userId;
+
+      @ApiModelProperty("商家持有的物品")
+      private Integer productId;
+
+
+}
